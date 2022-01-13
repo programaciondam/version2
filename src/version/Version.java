a23d65cb417a044c6ab6947f466359c05afe65b2
dam <dam@AULA005-PORTATIL1>
2022-01-13 09:52:06 +0100

    segundo commit

--- src/version/Version.java
+++ src/version/Version.java
@@ -16,6 +16,7 @@ public class Version {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
+        System.out.println("Segunda linea");
     }
     
 }
